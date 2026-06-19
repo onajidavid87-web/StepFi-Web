b3256d627ef1ab3391da3346f0abe807aa6518ab
Caesarr <onajidavid87@gmail.com>
2026-06-19 10:46:35 +0100

    fixed ci issues

--- src/components/docs/DocSidebar.tsx
+++ src/components/docs/DocSidebar.tsx
@@ -1,21 +1,5 @@
 import { clsx } from 'clsx'
-import { BookOpen, Rocket, DollarSign, Store, Award, Binary, Code } from 'lucide-react'
-
-export interface DocSection {
-  slug: string
-  title: string
-  icon: typeof BookOpen
-}
-
-export const DOC_SECTIONS: DocSection[] = [
-  { slug: 'introduction', title: 'Introduction', icon: BookOpen },
-  { slug: 'getting-started', title: 'Getting Started', icon: Rocket },
-  { slug: 'for-sponsors', title: 'For Sponsors', icon: DollarSign },
-  { slug: 'for-vendors', title: 'For Vendors', icon: Store },
-  { slug: 'for-mentors', title: 'For Mentors', icon: Award },
-  { slug: 'smart-contracts', title: 'Smart Contracts', icon: Binary },
-  { slug: 'api-reference', title: 'API Reference', icon: Code },
-]
+import { DOC_SECTIONS } from './DocSections'
 
 interface DocSidebarProps {
   activeSlug: string
